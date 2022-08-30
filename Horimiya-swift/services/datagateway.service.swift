b69965667129c58164b76a47c941f91609c83e0c
Emmanuel Mclean <emclean.dev@gmail.com>
2022-08-30 12:12:08 +0100

    Revert "+"
    This reverts commit d8a06f0f4f7217e7c3715479c253ea84496f5ea7.

--- Horimiya-swift/services/datagateway.service.swift
+++ Horimiya-swift/services/datagateway.service.swift
@@ -37,25 +37,9 @@ extension DataGateway {
     
 
     func save(habit: Habit) {
-        let request = NSFetchRequest<HabitEntity>(entityName: "HabitEntity")
-        request.predicate = NSPredicate(format: "identifier == %@", habit.id as CVarArg)
-        print("test1 start")
-
+        habit.getEntity(context: managedObjectContext)
         do {
-            let habitEntities = try managedObjectContext.fetch(request)
-            
-            if let habitEntity = habitEntities.first {
-                print("test1 found one to update")
-                habitEntity.title = habit.title
-                habitEntity.iconRef = habit.icon.rawValue
-                habitEntity.descript = habit.description
-                habitEntity.checkable = habit.checkable
-                habitEntity.schedule =  NSData.init(data: habit.scheduleToJson())
-            } else {
-                print("test1 creating new one")
-                habit.getEntity(context: managedObjectContext)
-            }
-
+            try managedObjectContext.save()
         } catch {
             print(error.localizedDescription)
         }
@@ -89,7 +73,7 @@ extension DataGateway {
  
     func edit(habit: Habit, set value: Any?, forKey key: String) {
         let request = NSFetchRequest<HabitEntity>(entityName: "HabitEntity")
-        request.predicate = NSPredicate(format: "identifier == %@", habit.id as CVarArg)
+        request.predicate = NSPredicate(format: "identifier == %@", habit.id)
         
         do {
             let habitEntities = try managedObjectContext.fetch(request)
@@ -111,7 +95,7 @@ extension DataGateway {
     
     func delete(habit: Habit) {
         let request = NSFetchRequest<HabitEntity>(entityName: "HabitEntity")
-        request.predicate = NSPredicate(format: "identifier == %@", habit.id as CVarArg)
+        request.predicate = NSPredicate(format: "identifier == %@", habit.id)
         
         do {
             let habitEntities = try managedObjectContext.fetch(request)
